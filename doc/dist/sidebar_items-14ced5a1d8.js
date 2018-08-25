fc091ef4abfe4b9f3a5398ce5afa10bf89dab5d2
Alberto Seoane <33812609+alchexmist@users.noreply.github.com>
2018-08-25 23:50:24 +0200

    Ex_doc docs

---
+++ doc/dist/sidebar_items-14ced5a1d8.js
@@ -0,0 +1,1 @@
+sidebarNodes={"extras":[{"id":"api-reference","title":"API Reference","group":"","headers":[{"id":"Modules","anchor":"modules"}]}],"exceptions":[],"modules":[{"id":"HelloAlchemy","title":"HelloAlchemy","group":"","functions":[{"id":"hello/1","anchor":"hello/1"}]}],"tasks":[]}
